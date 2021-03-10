7870d027301e75e32a1369133f05d97eddbabafb
xvandish <rsilva@psi.edu>
2021-03-09 23:28:32 -0700

    formatting

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const AWS = require('aws-sdk')
+const AWS = require("aws-sdk");
+const sharp = require("sharp");
+const util = require("util");
 const s3 = new AWS.S3();
 
 /**
- * 
- * @param {string} bucket - name of the bucket 
+ *
+ * @param {string} bucket - name of the bucket
  * @param {string} key - name of the key
  * @param {s3} s3 - instantiated s3 object
  */
@@ -11,42 +13,62 @@ async function getOriginalImage(bucket, key) {
   const imageLocation = { Bucket: bucket, Key: key };
   try {
     const originalImage = await s3.getObject(imageLocation).promise();
-    return originalImage; 
+    return originalImage;
   } catch (err) {
-    console.log(err)
-    return new Error('failed to download original image');
+    console.log(err);
+    return new Error("failed to download original image");
   }
 }
 
-async function resizeImage(photoWidth) { }
-async function uploadImageToS3() { } 
+async function resizeImage(photoWidth) {}
+async function uploadImageToS3() {}
 
-const PhotoSizes = {
-  "": { Width: 333 },
-  "@2x": { Width: 667 },
-  "_large": { Width: 1500 },
-  "_large@2x": { Width: 3000 }
-}
+const photoSizes = [
+  { prefix: "small", width: 333 },
+  { prefix: "@2x", width: 667 },
+  { prefix: "_large", width: 1500 },
+  { prefix: "_large@2x", Width: 3000 },
+];
+
+const supportedFormats = ["jpeg", "jpg", "png"];
 
 /**
- * 
+ *
  * @param {*} event an S3 Event Notification https://docs.aws.amazon.com/AmazonS3/latest/userguide/NotificationHowTo.html
  */
 exports.handler = async (event, context, callback) => {
-  console.log(event);
-  event.records.forEach(record => {
-    const srcBucket = record.s3.bucket.name;
-    const srcKey = record.s3.object.key; 
+  console.log("Reading options from event:\n", util.inspect(event, { depth: 5 }));
+  const record = event.Records[0];
+  const srcBucket = record.s3.bucket.name;
+  const srcKey = record.s3.object.key;
 
-    const originalImage = await getOriginalImage(srcBucket, srcKey);
-    if (originalImage instanceof Error) return;
+  // Get the extension from the file name
+  const typeMatch = srcKey.match(/\.([^.]*)$/);
+  if (!typeMatch) {
+    console.log("Could not determine the image type.");
+    return;
+  }
 
-    // Resize the image into each of the desired sizes
-     Object.keys(PhotoSizes).forEach(key => {
-       const resized = resizeImage(PhotoSizes[key].Width);
-       if (resized instanceof Error) return;
-       uploadImageToS3(resized);
-    }
+  // Check that the image type is supported
+  const imageType = typeMatch[1].toLowerCase();
+  if (!supportedFormats.includes(imageType)) {
+    console.log(`Unsupported image type: ${imageType}`);
+    return;
+  }
 
-  })
-}
+  const originalImage = await getOriginalImage(srcBucket, srcKey);
+  if (originalImage instanceof Error) return;
+
+  console.time("create initial sharp instance");
+  // Add rotation because even with EXIF metadata images come out roatated
+  // without it
+  const startingImage = await sharp(originalImage.Body).rotate();
+  console.timeEnd("create initial sharp instance");
+
+  photoSizes.forEach(async (key) => {
+    const resized = resizeImage(PhotoSizes[key].Width);
+    if (resized instanceof Error) return;
+    uploadImageToS3(resized);
+    s3.upload;
+  });
+};
--- script.js
+++ script.js
@@ -1,24 +1,38 @@
-const sharp = require('sharp');
+const sharp = require("sharp");
 
 // Testing compression for images, to see if either webp or avif are worth it
 
 const PhotoSizes = {
-  '': { Width: 333 },
-  '@2x': { Width: 667 },
+  small: { Width: 333 },
+  "@2x": { Width: 667 },
   _large: { Width: 1500 },
-  '_large@2x': { Width: 3000 },
+  "_large@2x": { Width: 3000 },
 };
-const fileName = 'soak';
+const fileName = "./IMG_1868.jpeg";
 async function test() {
-  console.time('pipeline init');
-  const pipeline = await sharp(`${fileName}.jpg`).rotate();
-  console.timeEnd('pipeline init');
+  console.time("pipeline init");
+  const pipeline = await sharp(fileName).rotate();
+  console.timeEnd("pipeline init");
+  console.time("test-timer");
   Object.keys(PhotoSizes).forEach(async (key) => {
     const width = PhotoSizes[key].Width;
-    console.time(`convert image ${key}`);
-    pipeline.resize(width).webp({ quality: 55 }).toFile(`${fileName}${key}.webp`);
-    console.timeEnd(`convert image ${key}`);
+    console.time(`convert image to 3 formats ${key}`);
+    const secondPipeline = pipeline.resize({
+      fit: "inside",
+      width,
+    });
+    await Promise.all([
+      // secondPipeline
+      //   .webp({ quality: 82 })
+      //   .toFile(`./output_photos/${fileName}${key}.webp`),
+      // secondPipeline.jpeg({ quality: 80 }).toFile(`./output_photos/${fileName}${key}.JPEG`),
+      secondPipeline
+        .avif({ quality: 64 })
+        .toFile(`./output_photos/${fileName}${key}.avif`),
+    ]);
+    console.timeEnd(`convert image to 3 formats ${key}`);
   });
+  console.timeEnd("test-timer");
 }
 
 test();
